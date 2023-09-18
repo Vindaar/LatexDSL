66b7326346d9fbf26e1b43d7604fb78707c94038
Vindaar <basti90@gmail.com>
2023-09-19 00:19:00 +0200

    adjust math delim test for pre 2.0 Nim versions

--- tests/tMathDelim.nim
+++ tests/tMathDelim.nim
@@ -4,7 +4,10 @@ import ../src/latexdsl
 
 ## NOTE: Because `MathDelim` is a {.compileTime.} variable, assigning to it at CT
 ## is done without static.
-MathDelim = "$$"
+when (NimMajor, NimMinor, NimPatch) > (1, 7, 0):
+  MathDelim = "$$"
+else:
+  static: MathDelim = "$$"
 
 suite "MathDelim changed in static context":
   test "`math` delimiter can be adjusted in `static` context":
