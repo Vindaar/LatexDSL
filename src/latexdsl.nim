228017c37447eb583eb0ba756a76e54df5505062
Vindaar <basti90@gmail.com>
2024-11-08 13:47:17 +0100

    conditionally import parts not supported on JS target
    Note: For some reason MathDelim has to be a runtime variable on the JS
    target. But I guess there it is much less of an issue / nuisance.
    It causes an internal compiler error otherwise.

--- src/latexdsl.nim
+++ src/latexdsl.nim
@@ -2,8 +2,11 @@ import macrocache
 # keep the value at 0 to get proper CT checks!
 const NoCommandChecks = CacheCounter"CommandCounterCheck"
 
-import latexdsl / [dsl_impl, latex_helpers, latex_compiler, valid_tex_commands]
+import latexdsl / [dsl_impl, latex_helpers, valid_tex_commands]
 export dsl_impl,
        valid_tex_commands,
-       latex_helpers,
-       latex_compiler
+       latex_helpers
+
+when not defined(js):
+  import latexdsl / latex_compiler
+  export latex_compiler
