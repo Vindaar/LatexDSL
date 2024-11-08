deab40b8efedd23a0d0aaa298f08a5a506f12b8a
Vindaar <basti90@gmail.com>
2024-11-08 13:48:09 +0100

    [tests] adjust test cases for JS target

--- tests/tMathDelim.nim
+++ tests/tMathDelim.nim
@@ -2,12 +2,15 @@ import unittest, strutils
 
 import ../src/latexdsl
 
-## NOTE: Because `MathDelim` is a {.compileTime.} variable, assigning to it at CT
-## is done without static.
-when (NimMajor, NimMinor, NimPatch) > (1, 7, 0):
-  MathDelim = "$$"
+when not defined(js):
+  ## NOTE: Because `MathDelim` is a {.compileTime.} variable, assigning to it at CT
+  ## is done without static.
+  when (NimMajor, NimMinor, NimPatch) > (1, 7, 0):
+    MathDelim = "$$"
+  else:
+    static: MathDelim = "$$"
 else:
-  static: MathDelim = "$$"
+  MathDelim = "$$" # runtime variable
 
 suite "MathDelim changed in static context":
   test "`math` delimiter can be adjusted in `static` context":
